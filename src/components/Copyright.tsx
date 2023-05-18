e2bf0389931dd4dcfe4623ae3d0b34d2feff4c3c
Priscilla Carrenho <priscillaonil@gmail.com>
2023-05-18 19:20:33 -0300

    first commit

---
+++ src/components/Copyright.tsx
@@ -0,0 +1,24 @@
+export function Copyright() {
+  return (
+    <div className="text-sm leading-relaxed text-gray-200">
+      Feito com 💜 por{" "}
+      <a
+        target="_blank"
+        rel="noreferger"
+        className="underline hover:text-gray-100"
+        href="http://pricarrenho.com.br"
+      >
+        Priscilla Carrenho
+      </a>{" "}
+      no NLW da{" "}
+      <a
+        target="_blank"
+        rel="noreferger"
+        className="underline hover:text-gray-100"
+        href="http://rocketseat.com.br"
+      >
+        Rocketseat
+      </a>
+    </div>
+  );
+}

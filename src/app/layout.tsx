94c089809e729c4ab61778fd5a0264dc7670184c
Priscilla Carrenho <priscillaonil@gmail.com>
2023-05-20 15:11:37 -0300

    feat: add memories components and route

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,12 @@ import {
   Bai_Jamjuree as BaiJamjuree,
 } from "next/font/google";
 
+import { SignIn } from "@/components/SignIn";
+import { Hero } from "@/components/Hero";
+import { Copyright } from "@/components/Copyright";
+import { Profile } from "@/components/Profile";
+import { cookies } from "next/headers";
+
 const roboto = Roboto({ subsets: ["latin"], variable: "--font-roboto" });
 const baiJamjuree = BaiJamjuree({
   subsets: ["latin"],
@@ -22,12 +28,34 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const isAuthenticated = cookies().has("token");
+
   return (
     <html lang="en">
       <body
         className={`${roboto.variable} ${baiJamjuree.variable} font-sans bg-gray-900 text-gray-100`}
       >
-        {children}
+        <main className="grid grid-cols-2 min-h-screen">
+          {/* Left */}
+          <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover px-28 py-16">
+            {/* Blur */}
+            <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
+
+            {/* Stripes */}
+            <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes " />
+
+            {isAuthenticated ? <Profile /> : <SignIn />}
+
+            <Hero />
+
+            <Copyright />
+          </div>
+
+          {/* Right */}
+          <div className="flex flex-col max-h-screen overflow-y-scroll  bg-[url(../assets/bg-stars.svg)] bg-cover">
+            {children}
+          </div>
+        </main>
       </body>
     </html>
   );
